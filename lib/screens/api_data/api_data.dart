7a4edb8c67357a1b82aa245709960048e7f8528e
Noukin <noukin68@gmail.com>
2023-07-16 19:26:22 +0300

    edit4

--- lib/screens/api_data/api_data.dart
+++ lib/screens/api_data/api_data.dart
@@ -1,34 +1,39 @@
 class ApiData {
-  static const String quizSyntax = 'http://localhost:5000/quizsyntax';
-  static const String quizTypeObject = 'http://localhost:5000/quiztypeobject';
+  static const String quizSyntax = 'http://62.217.182.138:3000/quizsyntax';
+  static const String quizTypeObject =
+      'http://62.217.182.138:3000/quiztypeobject';
   static const String quizTypeOperator =
-      'http://localhost:5000/quiztypeoperator';
-  static const String quizIfOperator = 'http://localhost:5000/quizifoperator';
-  static const String quizCycle = 'http://localhost:5000/quizcycle';
-  static const String quizStr = 'http://localhost:5000/quizstr';
-  static const String quizList = 'http://localhost:5000/quizlist';
-  static const String quizDictionary = 'http://localhost:5000/quizdictionary';
-  static const String quizSets = 'http://localhost:5000/quizsets';
-  static const String quizFunction = 'http://localhost:5000/quizfunction';
-  static const String quizExceptions = 'http://localhost:5000/quizexceptions';
-  static const String quizFile = 'http://localhost:5000/quizfile';
-  static const String quizModule = 'http://localhost:5000/quizmodule';
-  static const String quizRegular = 'http://localhost:5000/quizregular';
+      'http://62.217.182.138:3000/quiztypeoperator';
+  static const String quizIfOperator =
+      'http://62.217.182.138:3000/quizifoperator';
+  static const String quizCycle = 'http://62.217.182.138:3000/quizcycle';
+  static const String quizStr = 'http://62.217.182.138:3000/quizstr';
+  static const String quizList = 'http://62.217.182.138:3000/quizlist';
+  static const String quizDictionary =
+      'http://62.217.182.138:3000/quizdictionary';
+  static const String quizSets = 'http://62.217.182.138:3000/quizsets';
+  static const String quizFunction = 'http://62.217.182.138:3000/quizfunction';
+  static const String quizExceptions =
+      'http://62.217.182.138:3000/quizexceptions';
+  static const String quizFile = 'http://62.217.182.138:3000/quizfile';
+  static const String quizModule = 'http://62.217.182.138:3000/quizmodule';
+  static const String quizRegular = 'http://62.217.182.138:3000/quizregular';
 
   static const String practiceUrl =
       'https://www.programiz.com/python-programming/online-compiler/';
-  static const String requestUrl = 'http://localhost:5000/request';
-  static const String loginUrl = 'http://localhost:5000/login';
-  static const String adminRequests = 'http://localhost:5000/admin/requests';
-  static const String lectureScreen = 'http://localhost:5000/lectures';
-  static const String testScreen = 'http://localhost:5000/tests';
-  static const String addLecture = 'http://localhost:5000/addLecture';
+  static const String requestUrl = 'http://62.217.182.138:3000/request';
+  static const String loginUrl = 'http://62.217.182.138:3000/login';
+  static const String adminRequests =
+      'http://62.217.182.138:3000/admin/requests';
+  static const String lectureScreen = 'http://62.217.182.138:3000/lectures';
+  static const String testScreen = 'http://62.217.182.138:3000/tests';
+  static const String addLecture = 'http://62.217.182.138:3000/addLecture';
   static String approveRequest(int requestId) =>
-      'http://localhost:5000/admin/requests/$requestId/approve';
+      'http://62.217.182.138:3000/admin/requests/$requestId/approve';
   static String rejectRequest(int requestId) =>
-      'http://localhost:5000/admin/requests/$requestId/reject';
+      'http://62.217.182.138:3000/admin/requests/$requestId/reject';
   static String editLecture(int lectureId) =>
-      'http://localhost:5000/lectures/$lectureId';
+      'http://62.217.182.138:3000/lectures/$lectureId';
   static String deleteLecture(int lectureId) =>
-      'http://localhost:5000/lectures/$lectureId';
+      'http://62.217.182.138:3000/lectures/$lectureId';
 }
